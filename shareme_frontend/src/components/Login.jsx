77b5eda3418c035373625ec2d9af694830e8a92e
ajay <ajaydev299@gmail.com>
2024-08-10 20:32:58 +0530

    sanity done.Front end login started

---
+++ shareme_frontend/src/components/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { GoogleLogin } from '@react-oauth/google';
+import {FcGoogle} from 'react-icons/fc';
+import { useNavigate } from 'react-router-dom';
+const Login = () => {
+  return (
+    <div>
+      Login
+    </div>
+  )
+}
+
+export default Login
